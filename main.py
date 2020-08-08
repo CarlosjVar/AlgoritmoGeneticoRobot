abf7cc8c70ac0a6b5baa001d10b76b0f4369179c
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-08-08 11:33:30 -0600

    main.py cleaned

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-from models.Robot import Robot
 from controllers.mainController import MainController
-
-objetivo = (0,19)
-##TODO: IMPLEMENTAR MUTACIONES TOMANDO USANDO COMO EQUIVALENTE A LA SUBUNIDAD BIT LAS CELDAS DE LA MATRIZ DE COMPORTAMIENTOS
 
 
 # Main program
 if __name__ == "__main__":
     print("Algoritmo genetico robot...")
+    # Start controller
     main_controller = MainController()
     main_controller.run()
-    ###BOCETO
-    ###GENERACION
-    ###GENERACIONACTIVA = FUNC(GENERACION)
-    ###WHILE LEN(GENACTIVA) != 0:
-    ###FOR ROBOT IN GENERACION:
-    ###     ROBOT.ACCION
-    ###Genetics genetics()
-    ###for robot in gen:
-    ###    genetics.evaluar(robot)
-    ###nuevagen=genetics.generarGen
